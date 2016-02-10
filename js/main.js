b47dc3e26a318e40dc50d50ce68ea35885e9bdc7
许一鸣 <xuyiming@xuyimingdeMacBook-Pro.local>
2016-02-10 19:37:40 +0800

    Site updated: 2016-02-10 19:37:40

--- js/main.js
+++ js/main.js
@@ -89,11 +89,18 @@ require([], function (){
         ], function (ScrollReveal) {
           // 更多animation:
           // http://daneden.github.io/animate.css/
+/*          var animationNames = [*/
+            //"pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            //],
+            //len = animationNames.length,
+            //randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
           var animationNames = [
-            "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            "pulse"
             ],
             len = animationNames.length,
             randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
 
           // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
           if (!window.requestAnimationFrame) {
